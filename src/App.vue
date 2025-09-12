be2f7ec8500d762f59cf740833134d67ba1c766e
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2025-09-12 23:25:31 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-[#fafafa] min-h-screen">
+    <NavBar/>
+    <HeroSection/>
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const NavBar =defineAsyncComponent((  )=>import('@/components/NavBar.vue'))
+const HeroSection =defineAsyncComponent((  )=>import('@/components/HeroSection.vue'))
+</script>
