99157fe666adc87bc44585b7bea71ef2e3303a4f
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2025-09-15 01:39:49 +0800

    add footer section

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="font-mono mt-8 z-10 text-[#4d4d4d]">
+        <div class="container p-12 flex justify-between">
+            <div class="text-3xl font-bold">MJ/MC</div>
+            <p>©MJ/MC All Rights Reserved.</p>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
