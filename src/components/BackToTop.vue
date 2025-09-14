856b1b0efb3921a7f63de0e92749926c7df9c764
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2025-09-15 01:39:26 +0800

    add a component (backToTop) to smoothly scroll up from the top

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,24 @@
+<template>
+    <button v-if="isVisible"
+    @click="scrollToTop"
+    class="fixed bottom-4 right-4 bg-[#4d4d4d] text-[#fafafa] rounded-full w-12 h-12 flex items-center justify-center text-3xl font-mono cursor-pointer transition-opacity duration-300 hover:bg-[#4a5aec]">
+        ↑
+    </button>
+</template>
+
+<script setup>
+import { ref,onMounted, onUnmounted} from 'vue';
+const isVisible =ref(false);
+const handleScroll =()=>{
+    isVisible.value = window.scrollY > 300;
+}
+const scrollToTop =()=>{
+    window.scrollTo({top:0,behavior:'smooth'})
+}
+onMounted(()=>{
+    window.addEventListener('scroll',handleScroll);
+});
+onUnmounted(()=>{
+   window.removeEventListener('scroll',handleScroll);
+});
+</script>
