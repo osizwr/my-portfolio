1f76411201899a9607f38a08b30749a4b255e50a
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2025-09-15 01:38:59 +0800

    add loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="bg-[#fafafa] min-h-screen">
-    <NavBar/>
-    <HeroSection/>
-    <AboutSection/>
-    <ServicesSection/>
-    <ProjectSection/>
-    <ContactSection/>
+    <Suspense>
+      <template #default>
+        <div>
+          <NavBar/>
+          <HeroSection/>
+          <AboutSection/>
+          <ServicesSection/>
+          <ProjectSection/>
+          <ContactSection/>
+          <FooterSection/>
+          <BackToTop/>
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent } from 'vue';
-
 const NavBar =defineAsyncComponent((  )=>import('@/components/NavBar.vue'))
 const HeroSection =defineAsyncComponent((  )=>import('@/components/HeroSection.vue'))
 const AboutSection =defineAsyncComponent((  )=>import('@/components/AboutSection.vue'))
 const ServicesSection =defineAsyncComponent((  )=>import('@/components/ServicesSection.vue'))
 const ProjectSection =defineAsyncComponent((  )=>import('@/components/ProjectSection.vue'))
 const ContactSection =defineAsyncComponent((  )=>import('@/components/ContactSection.vue'))
+const FooterSection =defineAsyncComponent((  )=>import('@/components/FooterSection.vue'))
+const BackToTop =defineAsyncComponent((  )=>import('@/components/BackToTop.vue'))
+import LoadingSpinner from './components/LoadingSpinner.vue';
 </script>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex justify-center items-center h-full">
+        <svg class="animate-spin h-16 w-16 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <defs>
+                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                    <stop offset="0%" stop-color="#fafafa"/>
+                    <stop offset="100%" stop-color="#4d4d4d"/>
+                </linearGradient>
+            </defs>
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="url(#gradient1)" stroke-width="4" fill="none"></circle>
+            <path fill="#fafafa" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+        </svg>
+    </div>
+</template>
