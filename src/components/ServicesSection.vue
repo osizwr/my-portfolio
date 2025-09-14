30546318e054efe30707a01bdfbe0df26480df26
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2025-09-15 00:35:45 +0800

    fix logo (center) when using small device

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="text-[#4d4d4d] mt-20" id="services">
+    <section class="font-mono text-[#4d4d4d] mt-20" id="services">
         <div class="px-4 xl:pl-16">
             <h2 class="font-mono text-4xl font-bold text-[#4d4d4d] mb-4 text-center">My Services</h2>
         </div>
         <div class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-4">
             <div v-for="service in services" :key="service.id" data-aos="fade-up"
             class="px-8 py-12 rounded-xl bg-[#fafafa] shadow-lg border border-[#4d4d4d]">
-                <div class="mx-auto h-24 text-center xl:h-28 xl:w-28">
+                <div class="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
                     <div>
                         <img :src="service.icon" :alt="service.name">
                     </div>
